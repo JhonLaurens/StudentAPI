134e7ac4d5a3ac35b7576c7c96a29d8d6b2a75a6
jhonlaurens <jhonlaurens@gmail.com>
2023-10-22 00:32:48 -0500

    API Update: Enhancements have been implemented in the Student controller (StudentController) to handle CRUD operations more efficiently and securely. Validations have been added, and error handling has been improved to ensure optimal API performance. Additionally, detailed documentation has been added for each method. Please refer to individual method changes for more information.

---
+++ StudentAPI/StudentAPI/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<string> Greetings()
+        {
+            return "Hello World";
+        }
+        
+
+        //CRUD - Create, Read, Update, Delete
+        //Create - A a new record - POST
+        //Read - retrieve a single record or a list of records - GET
+        //Update - modify an existing record - PUT or PATCH
+        //Delete - remove an existing record - DELETE
+
+        //HTTP Verbs
+        //GET - Read
+        //POST - Create
+        //PUT or PATCH - Update
+        //DELETE - Delete
+
+
+    }
+}
---
+++ StudentAPI/StudentAPI/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentAPI.Models
+{
+    public class Student
+    {
+        //StudentId, Name, ContactNumber, Age
+        //Primary Key
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Name { get; set; } = ""; 
+        
+
+        public int ContactNumber { get; set; }
+
+        public int Age { get; set; }
+    }
+}
